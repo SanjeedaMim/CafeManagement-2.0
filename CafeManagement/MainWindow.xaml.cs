58449e1ff0774d293b34a3320efdc1a5ac568f0d
Sanjeeda Mim <72091175+SanjeedaMim@users.noreply.github.com>
2022-06-04 00:49:49 +0600

    Add project files.

---
+++ CafeManagement/MainWindow.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CafeManagement
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            btnExit.IsEnabled = true;
+            
+            txtPassword.Password = "031720";
+           
+        }
+
+        private void txtUserName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            txtUserName.Focus();
+        }
+
+        private void txtPassword_TextChanged(object sender, TextChangedEventArgs e)
+        {
+           txtPassword.Focus();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtUserName.Text == "Minifa" && txtPassword.Password == "031720") 
+            {
+                Dashboard ds = new Dashboard();
+                ds.BringIntoView();
+                this.Hide();
+            }
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
